9502ad8c6871ecd6ce644d77c1a90fe11cb505b4
artemonsh <shumeykoartem@mail.ru>
2023-01-20 15:43:45 +0300

    Добавлен 7ой урок

---
+++ Lesson_7/src/auth/schemas.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from fastapi_users import schemas
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    email: str
+    username: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    email: str
+    password: str
+    role_id: int
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
---
+++ Lesson_7/src/operations/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, MetaData
+
+metadata = MetaData()
+
+operation = Table(
+    "operation",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("quantity", String),
+    Column("figi", String),
+    Column("instrument_type", String, nullable=True),
+    Column("date", TIMESTAMP),
+    Column("type", String),
+)
