889e1dd15e7f03bebc9314c7cc0de7c99fd2b005
artemonsh <shumeykoartem@mail.ru>
2023-01-24 19:03:37 +0300

    Добавлен 9ый урок: Фоновые задачи с Celery, Redis и Flower

---
+++ Lesson_9/src/tasks/router.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, BackgroundTasks, Depends
+
+from auth.base_config import current_user
+
+from .tasks import send_email_report_dashboard
+
+router = APIRouter(prefix="/report")
+
+
+@router.get("/dashboard")
+def get_dashboard_report(background_tasks: BackgroundTasks, user=Depends(current_user)):
+    # 1400 ms - Клиент ждет
+    send_email_report_dashboard(user.username)
+    # 500 ms - Задача выполняется на фоне FastAPI в event loop'е или в другом треде
+    background_tasks.add_task(send_email_report_dashboard, user.username)
+    # 600 ms - Задача выполняется воркером Celery в отдельном процессе
+    send_email_report_dashboard.delay(user.username)
+    return {
+        "status": 200,
+        "data": "Письмо отправлено",
+        "details": None
+    }
---
+++ Lesson_9/src/tasks/tasks.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.message import EmailMessage
+
+from celery import Celery
+
+from config import SMTP_PASSWORD, SMTP_USER
+
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 465
+
+celery = Celery('tasks', broker='redis://localhost:6379')
+
+
+def get_email_template_dashboard(username: str):
+    email = EmailMessage()
+    email['Subject'] = 'Натрейдил Отчет Дашборд'
+    email['From'] = SMTP_USER
+    email['To'] = SMTP_USER
+
+    email.set_content(
+        '<div>'
+        f'<h1 style="color: red;">Здравствуйте, {username}, а вот и ваш отчет. Зацените 😊</h1>'
+        '<img src="https://static.vecteezy.com/system/resources/previews/008/295/031/original/custom-relationship'
+        '-management-dashboard-ui-design-template-suitable-designing-application-for-android-and-ios-clean-style-app'
+        '-mobile-free-vector.jpg" width="600">'
+        '</div>',
+        subtype='html'
+    )
+    return email
+
+
+@celery.task
+def send_email_report_dashboard(username: str):
+    email = get_email_template_dashboard(username)
+    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
+        server.login(SMTP_USER, SMTP_PASSWORD)
+        server.send_message(email)
